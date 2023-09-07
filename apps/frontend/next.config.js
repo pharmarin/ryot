7dcefe13b880b3bf9d7ef1cb92613de951be9ff1
pharmarin <15229104+pharmarin@users.noreply.github.com>
2023-09-07 12:47:27 +0000

    👌 IMPROVE: Custom tweaks

--- apps/frontend/next.config.js
+++ apps/frontend/next.config.js
@@ -1,22 +1,34 @@
-const withPWAInit = require("next-pwa");
-const runtimeCaching = require("./worker/cache");
+const withPWAInit = require('next-pwa');
+const runtimeCaching = require('./worker/cache');
 
 /** @type {import('next-pwa').PWAConfig} */
 const withPWA = withPWAInit({
-	dest: "public",
-	register: true,
-	skipWaiting: true,
-	disable: process.env.NODE_ENV === "development",
-	runtimeCaching,
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development',
+  runtimeCaching,
 });
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	eslint: { ignoreDuringBuilds: true },
-	output: "export",
-	reactStrictMode: true,
-	transpilePackages: ["@ryot/generated", "@ryot/graphql"],
-	typescript: { ignoreBuildErrors: true },
+  eslint: { ignoreDuringBuilds: true },
+  // output: "export",
+  reactStrictMode: true,
+  transpilePackages: ['@ryot/generated', '@ryot/graphql'],
+  typescript: { ignoreBuildErrors: true },
+  rewrites: async () => {
+    if (process.env.NODE_ENV !== 'development') {
+      return undefined;
+    }
+
+    return [
+      {
+        source: '/graphql/:path*',
+        destination: 'http://127.0.0.1:8000/graphql/:path*',
+      },
+    ];
+  },
 };
 
 module.exports = withPWA(nextConfig);
