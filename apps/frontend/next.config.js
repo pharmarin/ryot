6a53870df2e7bae856f623c99a0e15da068ebb67
pharmarin <15229104+pharmarin@users.noreply.github.com>
2023-09-07 20:00:50 +0000

    🐛 FIX: Rewrite should return empty array instead of undefined

--- apps/frontend/next.config.js
+++ apps/frontend/next.config.js
@@ -1,12 +1,12 @@
-const withPWAInit = require('next-pwa');
-const runtimeCaching = require('./worker/cache');
+const withPWAInit = require("next-pwa");
+const runtimeCaching = require("./worker/cache");
 
 /** @type {import('next-pwa').PWAConfig} */
 const withPWA = withPWAInit({
-  dest: 'public',
+  dest: "public",
   register: true,
   skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development',
+  disable: process.env.NODE_ENV === "development",
   runtimeCaching,
 });
 
@@ -15,17 +15,17 @@ const nextConfig = {
   eslint: { ignoreDuringBuilds: true },
   // output: "export",
   reactStrictMode: true,
-  transpilePackages: ['@ryot/generated', '@ryot/graphql'],
+  transpilePackages: ["@ryot/generated", "@ryot/graphql"],
   typescript: { ignoreBuildErrors: true },
   rewrites: async () => {
-    if (process.env.NODE_ENV !== 'development') {
-      return undefined;
+    if (process.env.NODE_ENV !== "development") {
+      return [];
     }
 
     return [
       {
-        source: '/graphql/:path*',
-        destination: 'http://127.0.0.1:8000/graphql/:path*',
+        source: "/graphql/:path*",
+        destination: "http://127.0.0.1:8000/graphql/:path*",
       },
     ];
   },
